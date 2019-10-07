e2e8cd2a9774315b841d5bd990ff643686dbf2df
Adam Finch <atyreefinch@gmail.com>
2019-10-07 09:45:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+    "My name is Adam"
+    end
+
+    get "/hometown" do
+    "My hometown is Nashville"
+    end
+
+    get "/favorite-song" do
+    "My favorite song is there's nowhere to go but UP"
+    end
+
 end
+
