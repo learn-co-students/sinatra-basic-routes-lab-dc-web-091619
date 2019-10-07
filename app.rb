f53abc741db18c9ef3acaadd8a4735251ed129ff
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-07 14:58:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Graham Flaspoehler"
+  end
+
+  get '/hometown' do 
+    "My hometown is Columbia, SC."
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Pop Goes the Weasel."
+  end
+  
 end
